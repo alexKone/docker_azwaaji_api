6a0b9bc789e280e89f6bdcc5a8e1198673913605
alexandrekone <kone.alexandr@gmail.com>
2022-11-25 16:03:36 +0100

    add UserLoginSubscriber

---
+++ project/src/EventSubscriber/UserLoginSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+class UserLoginSubscriber implements EventSubscriberInterface
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        /** @var User $user */
+        $user = $event->getAuthenticationToken()->getUser();
+        $user->setLastLogin(new \DateTime('now'));
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'security.interactive_login' => 'onSecurityInteractiveLogin',
+        ];
+    }
+}
