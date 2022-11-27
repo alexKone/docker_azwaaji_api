ad315fac10e1de0c3dfa6f96775397b9be5891b9
alexandrekone <kone.alexandr@gmail.com>
2022-11-27 10:32:19 +0100

    add data in token response

---
+++ project/src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event): void
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['data'] = [
+            'email' => $user->getUserIdentifier(),
+            'gender' => $user->getProfile()->getGender(),
+        ];
+
+        $event->setData($data);
+    }
+}
